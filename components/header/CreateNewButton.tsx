3c6059b00bd8069c2a8f6ceea15729222b5970c1
Mateusz Wiącek <68820849+MarioPeperoni@users.noreply.github.com>
2024-09-21 19:33:07 +0200

    style: fix styling on create new button

--- components/header/CreateNewButton.tsx
+++ components/header/CreateNewButton.tsx
@@ -6,7 +6,7 @@ const CreateNewButton = () => {
   return (
     <Link
       href={"/add-listing"}
-      className="group rounded-full border-4 border-white bg-boxit-primary px-5 py-3 font-bold transition-all duration-300 hover:border-8 hover:border-boxit-primary hover:bg-white hover:px-4 hover:py-2 md:rounded-sm"
+      className="group rounded-full border-8 border-transparent bg-boxit-primary px-3 py-1 font-bold transition-all duration-300 hover:border-8 hover:border-boxit-primary hover:bg-white md:rounded-sm"
     >
       <PiPlusBold size={24} className="block md:hidden" />
       <span className="hidden md:block">Add new listing</span>
